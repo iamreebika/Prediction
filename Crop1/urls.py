6f9588b80dbba1e88d26ebf66b9fa24aa20f3dc2
Reebika Bhatta <66770073+iamreebika@users.noreply.github.com>
2021-03-22 10:42:41 +0545

    Update urls.py

--- Crop1/urls.py
+++ Crop1/urls.py
@@ -1,16 +1,9 @@
 from django.urls import path
-
-
 from .views import index, predict_chances
-
 app_name='Crop1'
-
-
-
-
 
 urlpatterns=[
     path('', index, name='Crop1-name'),
     path('Crop1/',predict_chances,name='submit_prediction'),
-    #path('Crop1/results/',view_results,name='cresults'),
+    path('Crop1/results/',view_result,name='cresults'),
 ]
